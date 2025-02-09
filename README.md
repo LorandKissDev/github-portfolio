9f2fb0dd80b1b096d1053e38139ef08822426b22
Lorand Kiss <kisslorand127@gmail.com>
2025-02-09 10:09:16 +0100

    Final changes, translation

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-TODO:
-- további tartalom?
-    - meglévőhöz képek gyűjtése, elhelyezése
-- alap stílus
-- animációk
-- reszpo
-- optimalizálás
+# About
+My developer portfolio on a simple html page.
+
+# For development
+NPM install is required.        
+To use [vite](https://vite.dev/) in developnment mode, edit the index.html head section, comment out the bundle
+files and uncomment the rest. Finally, hit the `npm run dev` command.
--- resources/js/app.js
+++ resources/js/app.js
@@ -8,3 +8,9 @@ document.addEventListener('scroll', (e) => {
     fadeAnimations.handleScroll();
     galleryLoader.handleScroll();
 });
+
+//- Fixes lighthouse score, caused by this image incorrect dimension. I am hiding this image on mobile and
+// to prevent loading the resource, using an already requested one.
+if( (window.innerWidth || document.documentElement.clientWidth) <= 690 ){
+    document.querySelector('#about .sc_image img').setAttribute('width', 79);
+}
